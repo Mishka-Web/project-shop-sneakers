722753e73112612e0e8d5d41155837e9206a9fc6
Richer <81913556+Mishka-Web@users.noreply.github.com>
2023-11-11 12:32:39 +0500

    tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,21 @@ export default {
 		"./src/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				"gradient-lime": "linear-gradient(to top, #89F09C 0%, #3CC755 100%)"
+			}
+		},
+		colors: {
+			"DEFAULT": "#000000",
+			"green": {
+				500: "#A5D364",
+				600: "#8BB43C"
+			},
+			"red": {
+				"light": "#FF8585"
+			}
+		},
 		container: {
 			center: true,
 			padding: "15px"
