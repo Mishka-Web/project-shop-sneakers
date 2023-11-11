bcd63d429f7cdc8f922da4ba9ddd2a6bc7a19a47
Richer <81913556+Mishka-Web@users.noreply.github.com>
2023-11-11 22:27:08 +0500

    update main component

--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -2,10 +2,8 @@ export default function Main() {
 	return (
 		<main className="flex flex-col">
 			<div className="container">
-				{/* <div className="flex"></div> */}
-				<div className="flex justify-between flex-wrap gap-4">
+				<div className="flex items-center justify-between flex-wrap gap-4">
 					<h2>Все кроссовки</h2>
-					
 				</div>
 			</div>
 		</main>
