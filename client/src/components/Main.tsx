e8a34d0c0a334003ae9b0e2d4d6a5bcada4872c9
Richer <81913556+Mishka-Web@users.noreply.github.com>
2023-11-11 22:26:16 +0500

    add main component

---
+++ client/src/components/Main.tsx
@@ -0,0 +1,13 @@
+export default function Main() {
+	return (
+		<main className="flex flex-col">
+			<div className="container">
+				{/* <div className="flex"></div> */}
+				<div className="flex justify-between flex-wrap gap-4">
+					<h2>Все кроссовки</h2>
+					
+				</div>
+			</div>
+		</main>
+	);
+}
