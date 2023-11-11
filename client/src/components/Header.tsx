a4ae6151d43495837eee2fa1c7e6719350f6bab8
Richer <81913556+Mishka-Web@users.noreply.github.com>
2023-11-11 15:42:08 +0500

    create header component

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,9 +1,13 @@
+// import logo from '../assets/logo';
+
 export default function Header() {
 	return (
 		<>
 			<header>
 				<div className="container">
-					<div className="flex items-center gap-[1.6rem]"></div>
+					<div className="flex items-center gap-[1.6rem]">
+						{/* <img src={logo} alt="" /> */}
+					</div>
 				</div>
 			</header>
 		</>
