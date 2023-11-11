6631ebeab4baeb4609ed739e2e319c2d666ff9a9
Richer <81913556+Mishka-Web@users.noreply.github.com>
2023-11-12 04:17:38 +0500

    fix file App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,27 @@
 import Header from "./components/Header";
-import Main from "./components/Main";
 import SideMenu from "./components/SideMenu";
+import Search from "./components/Search";
+import Products from "./components/Products";
+import IntroSlider from "./components/IntroSlider";
 
 function App() {
 	return (
 		<>
 			<Header />
-			<Main />
+			<main className="flex flex-col pt-[4.5rem] pb-[7.2rem]">
+				<div className="container">
+					<IntroSlider />
+					<div className="flex flex-col">
+						<section className="flex items-start justify-between flex-wrap gap-4">
+							<h2 className="text-[3.2rem] font-bold">
+								Все кроссовки
+							</h2>
+							<Search />
+						</section>
+						<Products />
+					</div>
+				</div>
+			</main>
 			<SideMenu />
 		</>
 	);
