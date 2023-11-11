ea4668c783b593d9af1ccb96fd3461c5f544a88a
Richer <81913556+Mishka-Web@users.noreply.github.com>
2023-11-12 04:06:23 +0500

    add new page payment

---
+++ client/src/pages/PurchasesPage.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import Main from "../components/Main";
+import SideMenu from "../components/SideMenu";
+
+export default function PurchasesPage() {
+	return (
+		<>
+			<Header />
+			<Main />
+			<SideMenu />
+		</>
+	);
+}
