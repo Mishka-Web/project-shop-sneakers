fcfc5852c6bedb90753157d281b729d0fde1181a
Richer <81913556+Mishka-Web@users.noreply.github.com>
2023-11-12 04:56:49 +0500

    commited file models.ts

--- client/src/models.ts
+++ client/src/models.ts
@@ -1,10 +1,10 @@
-export interface IProduct {
-	id: number,
-	title: string
-}
+// export interface IProduct {
+// 	id: number,
+// 	title: string
+// }
 
-export interface IUser {
-	id: number,
-	login: string,
-	password: string
-}
+// export interface IUser {
+// 	id: number,
+// 	login: string,
+// 	password: string
+// }
