416ba699854e7dd2d50c0b91d057e29c923ddf1a
Richer <81913556+Mishka-Web@users.noreply.github.com>
2023-11-12 04:22:39 +0500

    add react-router-dom

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./assets/scss/index.scss";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<Router>
+		<App />
+	</Router>
+);
