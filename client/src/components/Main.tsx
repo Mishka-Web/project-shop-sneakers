ea2e5bb4872601341b7c71b4c95bac7797004220
Richer <81913556+Mishka-Web@users.noreply.github.com>
2023-11-11 22:51:13 +0500

    add component Search in Main.tsx

--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -1,9 +1,12 @@
+import Search from "./Search";
+
 export default function Main() {
 	return (
 		<main className="flex flex-col">
 			<div className="container">
 				<div className="flex items-center justify-between flex-wrap gap-4">
 					<h2>Все кроссовки</h2>
+					<Search />
 				</div>
 			</div>
 		</main>
