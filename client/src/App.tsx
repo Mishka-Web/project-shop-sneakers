a60568482f1e0fd7455a5cfeb1820c734443a932
Richer <81913556+Mishka-Web@users.noreply.github.com>
2023-11-12 03:30:13 +0500

    add new component sidemenu

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
+import SideMenu from "./components/SideMenu";
 
 function App() {
 	return (
 		<>
 			<Header />
 			<Main />
+			<SideMenu />
 		</>
 	);
 }
---
+++ client/src/components/SideMenu.tsx
@@ -0,0 +1,7 @@
+export default function SideMenu() {
+	return (
+		<aside className="fixed">
+			
+		</aside>
+	);
+}
