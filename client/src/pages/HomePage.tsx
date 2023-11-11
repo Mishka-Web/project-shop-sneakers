cdb89e3b7f6c4f689e391ef2322406089104c995
Richer <81913556+Mishka-Web@users.noreply.github.com>
2023-11-12 04:37:21 +0500

    add seo in HomePage.tsx

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,22 +1,29 @@
 import Search from "../components/Search";
 import Products from "../components/Products";
 import IntroSlider from "../components/IntroSlider";
+import { Helmet } from "react-helmet";
 
 export default function HomePage() {
 	return (
-		<main className="flex flex-col pt-[4.5rem] pb-[7.2rem]">
-			<div className="container">
-				<IntroSlider />
-				<div>
-					<section className="flex items-start justify-between flex-wrap gap-4">
-						<h2 className="text-[3.2rem] font-bold">
-							Все кроссовки
-						</h2>
-						<Search />
-					</section>
-					<Products />
+		<>
+			<Helmet>
+				<meta charSet="utf-8" />
+				<title>My Title</title>
+			</Helmet>
+			<main className="flex flex-col pt-[4.5rem] pb-[7.2rem]">
+				<div className="container">
+					<IntroSlider />
+					<div>
+						<section className="flex items-start justify-between flex-wrap gap-4">
+							<h2 className="text-[3.2rem] font-bold">
+								Все кроссовки
+							</h2>
+							<Search />
+						</section>
+						<Products />
+					</div>
 				</div>
-			</div>
-		</main>
+			</main>
+		</>
 	);
 }
