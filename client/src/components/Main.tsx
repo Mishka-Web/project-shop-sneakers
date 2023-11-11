b1fc2babd8df297670ce8fc8188a525c6dfa7412
Richer <81913556+Mishka-Web@users.noreply.github.com>
2023-11-12 03:27:41 +0500

    update main.tsx

--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -7,11 +7,15 @@ export default function Main() {
 		<main className="flex flex-col pt-[4.5rem] pb-[7.2rem]">
 			<div className="container">
 				<IntroSlider />
-				<div className="flex items-start justify-between flex-wrap gap-4">
-					<h2 className="text-[3.2rem] font-bold">Все кроссовки</h2>
-					<Search />
+				<div className="flex flex-col">
+					<section className="flex items-start justify-between flex-wrap gap-4">
+						<h2 className="text-[3.2rem] font-bold">
+							Все кроссовки
+						</h2>
+						<Search />
+					</section>
+					<Products />
 				</div>
-				<Products />
 			</div>
 		</main>
 	);
