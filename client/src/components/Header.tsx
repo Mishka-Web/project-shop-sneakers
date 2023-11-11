071acd904070bd3e7523b35605c6553f30555f08
Richer <81913556+Mishka-Web@users.noreply.github.com>
2023-11-11 15:33:37 +0500

    add dir components

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+	return (
+		<>
+			<header>
+				<div className="container">
+					<div className="flex items-center gap-[1.6rem]"></div>
+				</div>
+			</header>
+		</>
+	);
+}
