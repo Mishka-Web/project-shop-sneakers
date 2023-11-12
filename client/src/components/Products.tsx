19bd3f219a1e8446de011db0944af6287d291612
Richer <81913556+Mishka-Web@users.noreply.github.com>
2023-11-12 15:08:36 +0500

    update components

--- client/src/components/Products.tsx
+++ client/src/components/Products.tsx
@@ -6,7 +6,7 @@ export default function Products() {
 		<div className="mt-[3.6rem]">
 			<div className="grid grid-cols-[repeat(auto-fill,_minmax(26rem,_1fr))] gap-[4rem]">
 				{products.map((item) => (
-					<ProdCard key={item.id} data={item}/>
+					<ProdCard key={item.id} data={item} />
 				))}
 			</div>
 		</div>
