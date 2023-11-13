c2807fb9e009f20ca342233b7ff9112fe20dcb9a
Richer <81913556+Mishka-Web@users.noreply.github.com>
2023-11-13 13:56:08 +0500

    init GraphQL in server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,25 @@
 import express from 'express';
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	next();
-});
+let { graphqlHTTP } = require("express-graphql")
+let { buildSchema } = require("graphql");
 
-app.get('/', (req, res) => {});
+// Construct a schema, using GraphQL schema language
+let schema = buildSchema(`
+	type Query {
+		hello: String
+	}
+`);
+
+app.use(
+	"/graphql",
+	graphqlHTTP({
+		schema: schema,
+		rootValue: root,
+		graphiql: true,
+	})
+);
 
 app.listen(PORT, () => {
 	console.log(`Server listening on ${PORT}`);
