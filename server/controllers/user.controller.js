8b70cd21afbcb2bfb95a34c3757198157277bcd9
Richer <81913556+Mishka-Web@users.noreply.github.com>
2023-11-18 04:18:03 +0500

    add func login user

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -31,7 +31,16 @@ class UserController {
     }
 
     async login(req, res, next) {
-        
+        const {email, password} = req.body;
+        const user = await User.findOne({where: {email}});
+        if (!user) return next("Пользователь не найден!");
+        const comparePass = bcrypt.compareSync(password, user.password);
+        if (!comparePass) return next("Указан неверный пароль!");
+        const token = genJWT({
+            id: user.id,
+            email: user.email
+        });
+        return res.json({token});
     }
 
     async isAuth(req, res) {
