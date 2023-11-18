304e1d91cdb77d18610fbf60178fed3a152db735
Richer <81913556+Mishka-Web@users.noreply.github.com>
2023-11-18 17:17:24 +0500

    update client

--- client/src/components/Products.tsx
+++ client/src/components/Products.tsx
@@ -1,7 +1,14 @@
 import ProdCard from "./ProdCard";
-import products from "../products.json";
+// import products from "../products.json";
+import { getAll } from "../http/productAPI.js";
+import { useEffect, useState } from "react";
 
 export default function Products() {
+	const [products, setProducts] = useState([]);
+	useEffect(() => {
+		getAll.then((data) => setProducts(data));
+	});
+
 	return (
 		<div className="mt-[3.6rem]">
 			<div className="grid grid-cols-[repeat(auto-fill,_minmax(26rem,_1fr))] gap-[4rem]">
--- client/src/http/productAPI.js
+++
@@ -1,8 +0,0 @@
-import { $inst } from './index';
-import jwtDecode from 'jwt-decode';
-
-export default async function getAll() {
-	const { data } = await $inst.get('api/product/');
-	localStorage.setItem('token', data.token);
-	return jwtDecode(data.token);
-}
