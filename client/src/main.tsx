4a70f83302f76d83eccb7feb6bea5f73aca000a8
Richer <81913556+Mishka-Web@users.noreply.github.com>
2023-11-18 05:40:13 +0500

    add @types/node

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,10 @@ import App from "./App.jsx";
 import "./assets/scss/index.scss";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import { cfg } from './config/config.js';
+
+console.log(cfg.apiUrl);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<Router>
 		<App />
