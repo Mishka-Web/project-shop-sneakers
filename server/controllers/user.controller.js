13431584563fa6f47796a12c9b13550f01c5dc3e
Richer <81913556+Mishka-Web@users.noreply.github.com>
2023-11-19 02:37:51 +0500

    add auth page

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -21,7 +21,7 @@ class UserController {
         const user = await User.create({
             email, password: hashPass
         });
-        const basket = await Basket.create({userId: user.id});
+        await Basket.create({userId: user.id});
         const token = genJWT({
             id: user.id,
             email: email
@@ -33,13 +33,18 @@ class UserController {
     async login(req, res, next) {
         const {email, password} = req.body;
         const user = await User.findOne({where: {email}});
+
         if (!user) return next("Пользователь не найден!");
+
         const comparePass = bcrypt.compareSync(password, user.password);
+
         if (!comparePass) return next("Указан неверный пароль!");
+
         const token = genJWT({
             id: user.id,
             email: user.email
         });
+
         return res.json({token});
     }
 
@@ -48,6 +53,7 @@ class UserController {
             id: req.user.id,
             email: req.user.email
         });
+		
         return res.json({token});
     }
 }
