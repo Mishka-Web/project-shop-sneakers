33795481e174ff865a736957f2b94ac1865eda0b
Richer <81913556+Mishka-Web@users.noreply.github.com>
2023-11-20 00:42:06 +0500

    add component form

--- client/src/pages/AuthPage.tsx
+++ client/src/pages/AuthPage.tsx
@@ -1,88 +1,13 @@
-import { useState } from "react";
 import { Helmet } from "react-helmet";
+import FormAuth from "../components/FormAuth";
 
 export default function AuthPage() {
-	const [index, setIndex] = useState(0);
-
 	return (
 		<>
 			<Helmet>
 				<title>Авторизация</title>
 			</Helmet>
-			<div className="flex flex-col rounded-[1rem] overflow-hidden max-w-[55rem] w-full fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] shadow-[rgba(100,100,111,0.1)_0_0.7rem_2.9rem_0]">
-				<div className="flex flex-grow items-center">
-					<button
-						className={
-							index === 0
-								? "flex-[1_1_50%] h-[40px] bg-green-500 text-[#fff] transition-colors"
-								: "flex-[1_1_50%] h-[40px] hover:bg-[#f5f5f5] transition-colors"
-						}
-						type="button"
-						onClick={() => setIndex(0)}
-					>
-						Авторизация
-					</button>
-					<button
-						className={
-							index === 1
-								? "flex-[1_1_50%] h-[40px] bg-green-500 text-[#fff] transition-colors"
-								: "flex-[1_1_50%] h-[40px] hover:bg-[#f5f5f5] transition-colors"
-						}
-						type="button"
-						onClick={() => setIndex(1)}
-					>
-						Регистрация
-					</button>
-				</div>
-				<div className="flex flex-col p-[4.4rem_3.2rem_3.2rem]">
-					<div className={index === 0 ? "visible" : "hidden"}>
-						<form name="form-auth" className="flex flex-col gap-6">
-							<label className="flex flex-col gap-[0.6rem]">
-								Почта:
-								<input
-									className="inline-flex w-full h-[3.6rem] text-[1.4rem] px-5 outline-none border-b border-b-[#e4e4e4] transition-all focus:placeholder:opacity-0 placeholder:transition-all focus:border-b-[#222]"
-									name="email"
-									type="email"
-									placeholder="Введите email"
-								/>
-							</label>
-							<label className="flex flex-col gap-[0.6rem]">
-								Пароль:
-								<input
-									className="inline-flex w-full h-[3.6rem] text-[1.4rem] px-5 outline-none border-b border-b-[#e4e4e4] transition-all focus:placeholder:opacity-0 placeholder:transition-all focus:border-b-[#222]"
-									name="pass"
-									type="password"
-									placeholder="Введите пароль"
-								/>
-							</label>
-							<button className="flex items-center justify-center mt-[3.2rem] h-[3.6rem] bg-[#e4e4e4] rounded-[0.25rem] hover:text-[#fff] hover:bg-green-500 transition-all px-20 mx-auto max-w-[24.3rem] w-full" type="button">Войти</button>
-						</form>
-					</div>
-					<div className={index === 1 ? "visible" : "hidden"}>
-						<form name="form-reg" className="flex flex-col gap-6">
-							<label className="flex flex-col gap-[0.6rem]">
-								Почта:
-								<input
-									className="inline-flex w-full h-[3.6rem] text-[1.4rem] px-5 outline-none border-b border-b-[#e4e4e4] transition-all focus:placeholder:opacity-0 placeholder:transition-all focus:border-b-[#222]"
-									name="email"
-									type="email"
-									placeholder="Введите email"
-								/>
-							</label>
-							<label className="flex flex-col gap-[0.6rem]">
-								Пароль:
-								<input
-									className="inline-flex w-full h-[3.6rem] text-[1.4rem] px-5 outline-none border-b border-b-[#e4e4e4] transition-all focus:placeholder:opacity-0 placeholder:transition-all focus:border-b-[#222]"
-									name="pass"
-									type="password"
-									placeholder="Введите пароль"
-								/>
-							</label>
-							<button className="flex items-center justify-center mt-[3.2rem] h-[3.6rem] bg-[#e4e4e4] rounded-[0.25rem] hover:text-[#fff] hover:bg-green-500 transition-all px-20 mx-auto max-w-[24.3rem] w-full" type="button">Зарегистрироваться</button>
-						</form>
-					</div>
-				</div>
-			</div>
+			<FormAuth />
 		</>
 	);
 }
